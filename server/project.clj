c0689a85a2627f7251414db5b95f05785219861d
Buck Ryan <buck.ryan@gmail.com>
2013-05-16 20:50:42 -0400

    moved files and show latest entry

--- server/project.clj
+++ server/project.clj
@@ -7,6 +7,6 @@
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]
                  [korma "0.3.0-RC5"]]
   :plugins [[lein-ring "0.8.3"]]
-  :ring {:handler server.handler/app}
+  :ring {:handler server.routes/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
--- server/src/server/routes.clj
+++ server/src/server/routes.clj
@@ -1,10 +1,10 @@
-(ns server.handler
+(ns server.routes
   (:require [ring.util.response :refer [response]]
             [ring.middleware.json :as json]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.handler :as handler]
             [compojure.route :as route]
-            [korma.core]
+            [korma.core :refer :all]
             [korma.db :as db]))
 
 (db/defdb stand (db/postgres {:db "stand"
@@ -12,9 +12,16 @@
                               :password "password"
                               :host "127.0.0.1"}))
 
+(defn latest-entry []
+  (first
+    (select "events"
+      (fields :id :event :start_time :end_time)
+      (order :id :DESC)
+      (limit 1))))
+
 (defroutes app-routes
   (GET "/" []
-    (response (korma.core/select "events")))
+    (response (latest-entry)))
   (POST "/stand" []
     (response "coming..."))
   (route/resources "/")
