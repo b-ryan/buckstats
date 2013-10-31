e489dd54c9aa1aba695ff186805c2adcad419ae6
Buck Ryan <buck.ryan@gmail.com>
2013-10-30 23:55:00 -0400

    use a button to indicate whether standing

--- arduino/stand/stand.ino
+++ arduino/stand/stand.ino
@@ -1,20 +1,33 @@
-int GREEN = 4;
-int RED = 2;
+const int RED = 2;
+const int GREEN = 4;
+const int BUTTON = 6;
+
+boolean _standing = false;
+boolean _buttonBeingPressed = false;
+
+void setStanding(boolean standing) {
+  _standing = standing;
+  digitalWrite(_standing ? RED : GREEN, LOW);
+  digitalWrite(_standing ? GREEN : RED, HIGH);
+}
 
 void setup() {
-  pinMode(GREEN, OUTPUT);
   pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BUTTON, INPUT);
   Serial.begin(9600);
+  
+  setStanding(false);
 }
 
 void loop() {
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(RED, LOW);
-  
-  delay(500);
-  
-  digitalWrite(GREEN, LOW);
-  digitalWrite(RED, HIGH);
-  
-  delay(500);
+  if(digitalRead(BUTTON) == HIGH) {
+    if(!_buttonBeingPressed) {
+      setStanding(!_standing);
+      _buttonBeingPressed = true;
+    }
+  }
+  else {
+    _buttonBeingPressed = false;
+  }
 }
