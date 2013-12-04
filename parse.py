705518794aefc26d3396d8f57d68546411834173
Buck Ryan <buck.ryan@gmail.com>
2013-12-03 23:14:00 -0500

    fixed issues with the parser

--- parse.py
+++ parse.py
@@ -6,7 +6,6 @@ def demo_set():
     stand.save(('standing', '2013-12-02 23:50:30',))
     stand.save(('locked', '2013-12-02 23:51:00',))
     stand.save(('sitting', '2013-12-02 23:51:45',))
-    stand.save(('locked', '2013-12-02 23:52:00',))
     stand.save(('unlocked', '2013-12-02 23:52:10',))
     stand.save(('standing', '2013-12-02 23:52:50',))
     stand.save(('tracking_shutdown', '2013-12-02 23:52:50',))
@@ -18,18 +17,10 @@ connection = stand.get_connection()
 cursor = connection.cursor()
 cursor.execute('SELECT event, time FROM events ORDER BY time ASC')
 
-status = None
-start_time = None
+tracker = stand.Tracker()
 
 for event, time in cursor:
-    if event in ('sitting', 'standing',) and event != status:
-        if start_time:
-            print status, 'for', time - start_time
-        status = event
-        start_time = time
-    elif event in ('locked', 'tracking_shutdown',):
-        if start_time:
-            print status, 'for', time - start_time
-        start_time = None
-    elif event in ('unlocked',):
-        start_time = time
+    tracker.receive_event(event, time)
+
+for line in tracker.times:
+    print "{0:<10}\t{1}\t{2}".format(*line)
--- stand.py
+++ stand.py
@@ -11,6 +11,35 @@ import psycopg2
 
 ONE_YEAR = 60 * 60 * 24 * 365
 
+class Tracker:
+
+    def __init__(self):
+        self.times = []
+        self.position = 'sitting'
+        self.lock_status = 'tracking_shutdown'
+
+    def receive_event(self, event, time):
+        last_row = self.times[-1] if len(self.times) else None
+
+        if last_row and not last_row[2]:
+            ## there is a sitting/standing event waiting to be closed
+            # no matter what the event, we need to close that one out
+            last_row[2] = time
+            self.times[-1] = last_row
+
+        if event in ('sitting', 'standing',):
+            if self.lock_status != 'locked':
+                self.times.append([event, time, None])
+            self.position = event
+
+        elif event == 'unlocked':
+            assert(last_row)
+            self.times.append([self.position, time, None])
+            self.lock_status = event
+
+        elif event in ('locked', 'tracking_shutdown',):
+            self.lock_status = event
+
 def dt():
     return time.strftime('%Y-%m-%d %H:%M:%S')
 
