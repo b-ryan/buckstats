214c8bc83b8dc1ffe012d11148985390918449d9
Buck Ryan <buck.ryan@gmail.com>
2013-12-07 16:00:52 -0500

    the download_weights script now successfully saves to the database

--- scripts/download_weights.py
+++ scripts/download_weights.py
@@ -6,6 +6,7 @@ _dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(_dir, '..'))
 
 from stand.model import Weight
+from stand.app import db
 
 login = sys.argv[1]
 pw = sys.argv[2]
@@ -15,11 +16,15 @@ gc = gspread.login(login, pw)
 sh = gc.open('Weight')
 worksheet = sh.worksheet('Weight')
 
+Weight.query.delete()
+
 for value in worksheet.get_all_values()[1:]:
     weight = Weight(
         date=value[0],
-        weight=value[2],
-        goal_weight=value[1],
-        notes=value[3],
+        weight=float(value[2]),
+        goal_weight=(float(value[1]) if value[1] != '' else None),
+        notes=(value[3] if value[3] != '' else None),
     )
-    print weight
+    db.session.add(weight)
+
+db.session.commit()
