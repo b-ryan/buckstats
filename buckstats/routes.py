4be7907575d5adf862cf0e276bc2a5946721c941
Buck Ryan <fryan@rjmetrics.com>
2013-12-12 18:22:04 -0500

    created an endpoint to refresh google data

--- buckstats/routes.py
+++ buckstats/routes.py
@@ -2,6 +2,8 @@ from buckstats.app import app, db
 import buckstats.model as m
 import flask
 from flask.ext.restless import APIManager
+import gspread
+import config
 
 # this will create routes at /api/
 api = APIManager(app, flask_sqlalchemy_db=db)
@@ -12,3 +14,25 @@ api.create_api(
     results_per_page=None,
     allow_functions=True,
 )
+
+@app.route('/api/weights/refresh', methods=['POST'])
+def refresh_weights():
+    gc = gspread.login(config.gmail_user, config.gmail_pass)
+
+    sh = gc.open('Weight')
+    worksheet = sh.worksheet('Weight')
+
+    m.Weight.query.delete()
+
+    for value in worksheet.get_all_values()[1:]:
+        weight = m.Weight(
+            date=value[0],
+            weight=float(value[2]),
+            goal_weight=(float(value[1]) if value[1] != '' else None),
+            notes=(value[3] if value[3] != '' else None),
+        )
+        db.session.add(weight)
+
+    db.session.commit()
+
+    return {}
