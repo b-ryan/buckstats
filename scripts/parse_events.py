fae5a1ad54323683642664c5e56c67bcc9f5e018
Buck Ryan <fryan@rjmetrics.com>
2013-12-18 14:31:21 -0500

    fixed the parser

--- scripts/parse_events.py
+++ scripts/parse_events.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
-import stand
+import work_listener as w
+import sys
+
+delim = "\t" if len(sys.argv) == 1 else sys.argv[1]
 
 def demo_set():
-    stand.save(('sitting', '2013-12-02 23:50:00',))
-    stand.save(('standing', '2013-12-02 23:50:30',))
-    stand.save(('locked', '2013-12-02 23:51:00',))
-    stand.save(('sitting', '2013-12-02 23:51:45',))
-    stand.save(('unlocked', '2013-12-02 23:52:10',))
-    stand.save(('standing', '2013-12-02 23:52:50',))
-    stand.save(('tracking_shutdown', '2013-12-02 23:52:50',))
-    stand.save(('standing', '2013-12-02 23:52:55',))
-    stand.save(('sitting', '2013-12-02 23:54:00',))
-    stand.save(('tracking_shutdown', '2013-12-02 23:55:00',))
+    w.save(('sitting', '2013-12-02 23:50:00',))
+    w.save(('standing', '2013-12-02 23:50:30',))
+    w.save(('locked', '2013-12-02 23:51:00',))
+    w.save(('sitting', '2013-12-02 23:51:45',))
+    w.save(('unlocked', '2013-12-02 23:52:10',))
+    w.save(('standing', '2013-12-02 23:52:50',))
+    w.save(('tracking_shutdown', '2013-12-02 23:52:50',))
+    w.save(('standing', '2013-12-02 23:52:55',))
+    w.save(('sitting', '2013-12-02 23:54:00',))
+    w.save(('tracking_shutdown', '2013-12-02 23:55:00',))
 
-connection = stand.get_connection()
+connection = w.get_connection()
 cursor = connection.cursor()
 cursor.execute('SELECT event, time FROM events ORDER BY time ASC')
 
-tracker = stand.Tracker()
+tracker = w.Tracker()
 
 for event, time in cursor:
     tracker.receive_event(event, time)
 
-for line in tracker.times:
-    print "{0:<10}\t{1}\t{2}".format(*line)
+for i, line in enumerate(tracker.times):
+    print delim.join(map(str, [i] + line))
