d56fe201bc39a2d8cd72f79e69aeed0fe045becd
Buck Ryan <buck.ryan@gmail.com>
2014-01-15 00:19:11 -0500

    changed the work_listener to send events to the api

--- sensors/parse_events.py
+++ sensors/parse_events.py
@@ -1,26 +1,50 @@
 #!/usr/bin/env python
-import work_listener as w
 import sys
 
 delim = "\t" if len(sys.argv) == 1 else sys.argv[1]
 
-def demo_set():
-    w.save(('sitting', '2013-12-02 23:50:00',))
-    w.save(('standing', '2013-12-02 23:50:30',))
-    w.save(('locked', '2013-12-02 23:51:00',))
-    w.save(('sitting', '2013-12-02 23:51:45',))
-    w.save(('unlocked', '2013-12-02 23:52:10',))
-    w.save(('standing', '2013-12-02 23:52:50',))
-    w.save(('tracking_shutdown', '2013-12-02 23:52:50',))
-    w.save(('standing', '2013-12-02 23:52:55',))
-    w.save(('sitting', '2013-12-02 23:54:00',))
-    w.save(('tracking_shutdown', '2013-12-02 23:55:00',))
+class Tracker:
 
-connection = w.get_connection()
+    def __init__(self):
+        self.times = []
+        self.position = 'sitting'
+        self.lock_status = 'tracking_shutdown'
+
+    def receive_event(self, event, time):
+        last_row = self.times[-1] if len(self.times) else None
+
+        if last_row and not last_row[2]:
+            ## there is a sitting/standing event waiting to be closed
+            # no matter what the event, we need to close that one out
+            last_row[2] = time
+            self.times[-1] = last_row
+
+        if event in ('sitting', 'standing',):
+            if self.lock_status != 'locked':
+                self.times.append([event, time, None])
+            self.position = event
+
+        elif event == 'unlocked':
+            assert(last_row)
+            self.times.append([self.position, time, None])
+            self.lock_status = event
+
+        elif event in ('locked', 'tracking_shutdown',):
+            self.lock_status = event
+
+def get_connection():
+    return psycopg2.connect(
+        host='127.0.0.1',
+        database='stand',
+        user='stand',
+        password='password',
+    )
+
+connection = get_connection()
 cursor = connection.cursor()
 cursor.execute('SELECT event, time FROM events ORDER BY time ASC')
 
-tracker = w.Tracker()
+tracker = Tracker()
 
 for event, time in cursor:
     tracker.receive_event(event, time)
--- sensors/work_listener.py
+++ sensors/work_listener.py
@@ -1,64 +1,29 @@
 #!/usr/bin/env python
 import Queue
 import logging
-import psycopg2
 import os
 import requests
 import sensor_threads
 import argparse
+import json
 
 BUCKSTATS_TOKEN = os.environ['BUCKSTATS_TOKEN']
 
 ONE_YEAR = 60 * 60 * 24 * 365
 
-class Tracker:
-
-    def __init__(self):
-        self.times = []
-        self.position = 'sitting'
-        self.lock_status = 'tracking_shutdown'
-
-    def receive_event(self, event, time):
-        last_row = self.times[-1] if len(self.times) else None
-
-        if last_row and not last_row[2]:
-            ## there is a sitting/standing event waiting to be closed
-            # no matter what the event, we need to close that one out
-            last_row[2] = time
-            self.times[-1] = last_row
-
-        if event in ('sitting', 'standing',):
-            if self.lock_status != 'locked':
-                self.times.append([event, time, None])
-            self.position = event
-
-        elif event == 'unlocked':
-            assert(last_row)
-            self.times.append([self.position, time, None])
-            self.lock_status = event
-
-        elif event in ('locked', 'tracking_shutdown',):
-            self.lock_status = event
-
-def get_connection():
-    return psycopg2.connect(
-        host='127.0.0.1',
-        database='stand',
-        user='stand',
-        password='password',
+def save(message):
+    event = {
+        'event': message[0],
+        'time': message[1],
+    }
+    requests.post(
+        'http://localhost:5000/api/events',
+        data=json.dumps(event),
+        headers={
+            'content-type': 'application/json',
+            'token': BUCKSTATS_TOKEN,
+        },
     )
-
-def save(event):
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute('''
-        INSERT into events (event, time)
-        VALUES (%s, %s)
-        ''',
-        event
-    )
-    connection.commit()
-    connection.close()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
@@ -81,6 +46,7 @@ if __name__ == '__main__':
 
     logging.info('Reading from queue')
 
+    save(sensor_threads.msg('startup'))
     try:
         while True:
             # queue.get blocks Ctrl-C signal unless a timeout is
@@ -89,4 +55,4 @@ if __name__ == '__main__':
             logging.debug('Received message ' + str(message))
             save(message)
     finally:
-        save(sensor_threads.msg('tracking_shutdown'))
+        save(sensor_threads.msg('shutdown'))
